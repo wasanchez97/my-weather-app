fae45a45f7dfc44822d8b27ef280d528a0a24ce5
wasanchez97 <175385472+wasanchez97@users.noreply.github.com>
2024-08-03 10:13:34 -0500

    Update backgrounds.js

--- src/utils/backgrounds.js
+++ src/utils/backgrounds.js
@@ -1,45 +1,45 @@
 
 const backgrounds = {
     Clear: {
-      day: '/src/img/skyclear.jpg',
-      night: '/src/img/clearnigth.jpg'
+      day: 'src/img/skyclear.jpg',
+      night: 'src/img/clearnigth.jpg'
     },
     Clouds: {
-      day: '/src/img/brokenclouds.jpg',
-      night: '/src/img/brokenigth.jpg'
+      day: 'src/img/brokenclouds.jpg',
+      night: 'src/img/brokenigth.jpg'
     },
     Rain: {
-      day: '/src/img/rain.jpg',
-      night: '/src/img/rainnigth.jpg'
+      day: 'src/img/rain.jpg',
+      night: 'src/img/rainnigth.jpg'
     },
     Drizzle: {
-      day: '/src/img/drizzle.jpg',
-      night: '/src/img/drizzlenigth.jpg'
+      day: 'src/img/drizzle.jpg',
+      night: 'src/img/drizzlenigth.jpg'
     },
     Thunderstorm: {
-      day: '/src/img/thunder.jpg',
-      night: '/src/img/thundernigth.jpg'
+      day: 'src/img/thunder.jpg',
+      night: 'src/img/thundernigth.jpg'
     },
     Snow: {
       day: '/src/img/snow.jpg',
       night: '/src/img/snownight.jpg'
     },
     Mist: {
-      day: '/src/img/mist.jpg',
-      night: '/src/img/mistnigth.jpg'
+      day: 'src/img/mist.jpg',
+      night: 'src/img/mistnigth.jpg'
     },
     
     Fog: {
-      day: '/src/img/fog.jpeg',
-      night: '/src/img/fognigth.jpg'
+      day: 'src/img/fog.jpeg',
+      night: 'src/img/fognigth.jpg'
     },
     Extreme: {
-      day: '/src/img/extreme.jpg', // Fondo genérico para condiciones extremas
-      night: '/src/img/extreme.jpg' // Puedes usar el mismo fondo o uno diferente
+      day: 'src/img/extreme.jpg', // Fondo genérico para condiciones extremas
+      night: 'src/img/extreme.jpg' // Puedes usar el mismo fondo o uno diferente
     },
     Additional: {
-      day: '/src/img/additional.jpg', // Fondo genérico para condiciones adicionales
-      night: '/src/img/additional.jpg' // Puedes usar el mismo fondo o uno diferente
+      day: 'src/img/additional.jpg', // Fondo genérico para condiciones adicionales
+      night: 'src/img/additional.jpg' // Puedes usar el mismo fondo o uno diferente
     }
 
 
